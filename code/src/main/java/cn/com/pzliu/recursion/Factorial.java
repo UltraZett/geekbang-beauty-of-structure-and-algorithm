d1ed43eba8d9d55c708d3ce230224fcc972e73da
UltraZett <pzliu5243@163.com>
2022-10-07 11:04:37 +0800

    阶乘的实现 全排列的实现

--- code/src/main/java/cn/com/pzliu/recursion/Factorial.java
+++ code/src/main/java/cn/com/pzliu/recursion/Factorial.java
@@ -4,4 +4,27 @@ package cn.com.pzliu.recursion;
  * 求阶乘
  */
 public class Factorial {
+
+    public long cal(int n){
+
+        if (n < 0){
+            throw new IllegalArgumentException("传入参数错误");
+        }
+
+        if (n == 0){
+            return 1;
+        }
+
+        return n * cal(n - 1);
+    }
+
+
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        System.out.println(factorial.cal(0));
+        System.out.println(factorial.cal(1));
+        System.out.println(factorial.cal(3));
+        System.out.println(factorial.cal(10));
+    }
+
 }
--- code/src/main/java/cn/com/pzliu/recursion/FullArrangement.java
+++ code/src/main/java/cn/com/pzliu/recursion/FullArrangement.java
@@ -1,7 +1,47 @@
 package cn.com.pzliu.recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 全排列
  */
 public class FullArrangement {
+
+
+    public List<List<Integer>> cal(List<Integer> list){
+        ArrayList<List<Integer>> result = new ArrayList<>();
+
+        if (list.size() == 1){
+            return Arrays.asList(list);
+        }
+
+
+        for (Integer num : list) {
+            List<Integer> collect = list.stream().filter(x -> !x.equals(num)).collect(Collectors.toList());
+            if (!collect.isEmpty()){
+                List<List<Integer>> subArrangement = cal(collect);
+                subArrangement.forEach(l->{
+                    List<Integer> subResult = new ArrayList<>();
+                    subResult.add(num);
+                    subResult.addAll(l);
+                    result.add(subResult);
+                });
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        FullArrangement fullArrangement = new FullArrangement();
+        System.out.println(fullArrangement.cal(Arrays.asList(1, 2)));
+        System.out.println(fullArrangement.cal(Arrays.asList(1, 2,3)));
+        System.out.println(fullArrangement.cal(Arrays.asList(1, 2,3,4)));
+    }
+
 }
